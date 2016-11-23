f20ed381e711930c9fd63724ac8a3f53a4ff16a8
Prachi Bhagdikar <pbhagdikar@digitsexperts.com>
2016-11-23 17:16:22 +0530

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post "/sort" => "blogs#sort"
 
-
+   match "/blogs/:id/delete", :to => "blogs#destroy", :via => ["delete"]
 end
